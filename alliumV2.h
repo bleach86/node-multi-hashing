3d261fd8bb75ef56d0589ab61d00ca3fa1e27f91
bleach86 <josh.leach86@yahoo.com>
2018-09-23 13:12:53 -0500

    add alliumV2

---
+++ alliumV2.h
@@ -0,0 +1,17 @@
+
+#ifndef ALLIUMV2_H
+#define ALLIUMV2_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+void alliumV2_hash(const char* input, char* output);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
